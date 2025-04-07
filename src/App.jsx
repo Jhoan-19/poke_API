b2ed505016ddde9c8f639c05aabdd97dd49adcd1
Jhoan-19 <jhoanricardov11@gmail.com>
2025-04-07 20:04:37 +0000

    profesional

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,32 @@
 import { useState } from 'react'
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-
 import './App.css'
+
 import Aleatorios from './Componentes/aleatorios'
-import Capturados from './Componentes/capturados'
+import  Capturados from './Componentes/capturados'  
 import Favoritos from './Componentes/favoritos'
-import Lista from './Componentes/lista'
+import Lista from './Componentes/lista' 
 import Pokemon from './Componentes/pokemon'
-import Usuarios from './Componentes/usuarios'
+import Usuario from './Componentes/usuarios'
 
 function App() {
 
   return (
     <Router>
 
-    <Menu />
     
-    <Routes>
-      <Route path="/" element={<Lista/>} />
-    </Routes>
-  
-  </Router>
+
+
+<Routes>
+  <Route path="/" element={<Lista />} />
+  <Route path="/usuarios" element={<Usuario />} />
+  <Route path="/aleatorios" element={<Aleatorios />} />
+  <Route path="/capturados" element={<Capturados />} />
+  <Route path="/favoritos" element={<Favoritos />} />
+</Routes>
+
+</Router>
+ 
   )
 }
 
